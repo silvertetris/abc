bc9312241f035bb4e3b437edeb44080a209f8b49
silvertetris <dltjdrnjs3@naver.com>
2023-02-17 15:49:28 +0900

    thread .join()

---
+++ src/main/java/org/abc/Test07.java
@@ -0,0 +1,18 @@
+package org.abc;
+
+public class Test07 {
+    public static void main(String[] args) {
+        Phone calling = new Phone();
+        calling.start();
+
+        try {
+            calling.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for(int i=1; i<=1000; i++) {
+            System.out.println("음식 먹기 : " +i);
+        }
+    }
+}
