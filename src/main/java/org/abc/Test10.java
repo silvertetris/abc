ab6291a601fb206b50d5e25d36ef59bf65b8d50d
silvertetris <dltjdrnjs3@naver.com>
2023-02-17 16:28:19 +0900

    Future interface

--- src/main/java/org/abc/Test10.java
+++ src/main/java/org/abc/Test10.java
@@ -1,4 +1,31 @@
 package org.abc;
 
+import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Test10 {
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        Future<Date> future = threadPool.submit(new Callable<Date>() {
+
+            @Override
+            public Date call() throws Exception {
+                Thread.sleep(1000);
+                return new Date();
+            }
+        });
+        threadPool.shutdown();
+
+
+        Date date = null;
+        try {
+            date = future.get();
+            System.out.println(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
