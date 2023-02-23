f319d96ee7e7736fe9480c1550eeb5f01801d593
silvertetris <dltjdrnjs3@naver.com>
2023-02-23 15:18:44 +0900

    Hashset override

---
+++ src/main/java/org/abc/CollectionApi/Test11.java
@@ -0,0 +1,50 @@
+package org.abc.CollectionApi;
+
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+class User {
+    String ssn;
+    String name;
+
+    User(String ssn, String name) {
+        this.ssn=ssn;
+        this.name=name;
+    }
+
+    @Override
+    public String toString() {
+        return ssn+":"+name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.parseInt(ssn);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean result= false;
+        User u=(User) obj;
+        if(this.ssn.equals(u.ssn))
+            result = true;
+        return result;
+    }
+}
+public class Test11 {
+
+    public static void main(String[] args) {
+        User u1= new User("123", "김푸름");
+        User u2=new User("123", "김푸름");
+
+        HashSet<User> users=new HashSet<>();
+
+        users.add(u1);
+        users.add(u2);
+
+        Iterator<User> iter=users.iterator();
+        while (iter.hasNext())
+            System.out.println(iter.next());
+    }
+}
